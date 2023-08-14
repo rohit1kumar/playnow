ae1f0f560dba1112f407e3710e64b723ee554754
Rohit Kumar <kumar1rohit@outlook.com>
2023-08-14 17:46:56 +0530

    add navbar and seach button

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { logo } from '../utils/constants'
+import { Search } from '@mui/icons-material'
+import SearchBar from './SearchBar'
+
+const NavBar = () => {
+    return (
+        <Stack
+            direction={'row'}
+            alignItems={'center'}
+            p={2}
+            sx={{
+                position: 'sticky',
+                top: 0,
+                justifyContent: 'space-between',
+                backgroundColor: '#000'
+            }}
+        >
+            <Link
+                to="/"
+                style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={logo} alt="logo" height={45} />
+            </Link>
+            <SearchBar />
+        </Stack>
+    )
+}
+
+export default NavBar
