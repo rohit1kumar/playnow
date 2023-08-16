d5e53b4b548c037b31a03f78b87b318a3ae8d343
Rohit Kumar <kumar1rohit@outlook.com>
2023-08-16 22:29:26 +0530

    add code spliting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Box } from "@mui/material"
-import {
-  Feed,
-  NavBar,
-  SearchFeed,
-  VideoDetails,
-  ChannelDetails,
-} from "./components"
+import { NavBar } from "./components"
+
+const Feed = lazy(() => import("./components/Feed"));
+const VideoDetails = lazy(() => import("./components/VideoDetails"));
+const ChannelDetails = lazy(() => import("./components/ChannelDetails"));
+const SearchFeed = lazy(() => import("./components/SearchFeed"));
 
 function App() {
 
@@ -15,12 +15,14 @@ function App() {
       <Box sx={{ backgroundColor: 'primary.main' }}>
         <NavBar />
       </Box>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetails />} />
         <Route path="/channel/:id" element={<ChannelDetails />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
